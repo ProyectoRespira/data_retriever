0db1dd53b7d42cd30ba6d5a985c882828b07a420
quanturtle <alvaromachucar@gmail.com>
2024-06-06 08:26:05 -0400

    added resource for credentials

--- etl-process/etl_process/__init__.py
+++ etl-process/etl_process/__init__.py
@@ -1,11 +1,18 @@
 from dagster import Definitions, load_assets_from_modules
 
-from etl_process import assets, jobs
+from etl_process import assets, jobs, resources
 
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
-    jobs=[jobs.run_pipeline]
+    jobs=[jobs.run_pipeline],
+    resources={
+        "credentials": resources.SomeCredentials(
+            user="user",
+            password="password",
+            host="host",
+        )
+    }
 )
--- etl-process/etl_process/assets.py
+++ etl-process/etl_process/assets.py
@@ -3,14 +3,17 @@ from typing import List
 
 from dagster import asset, AssetExecutionContext
 
+from etl_process.resources import SomeCredentials
+
 
 @asset(
     description="Extract data from source",
     compute_kind="python",
     group_name="extract"
 )
-def select_data(context: AssetExecutionContext):
+def select_data(context: AssetExecutionContext, credentials: SomeCredentials):
     context.log.info("Extracting data from source")
+    context.log.info(f"User: {credentials.user} | Password: {credentials.password} | Host: {credentials.host}")
     return "step 1"
 
 
