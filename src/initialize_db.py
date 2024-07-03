75c301a8ab5277fe212c99e8ec69e32ddb3bfa08
vnbl <fernanda.carles@gmail.com>
2024-07-03 15:45:30 -0400

    added functions to create weather and pattern stations from json

--- src/initialize_db.py
+++ src/initialize_db.py
@@ -1,4 +1,4 @@
-from src.models import BasePostgres, Stations, Regions
+from src.models import BasePostgres, Stations, Regions, WeatherStations, PatternStations
 from sqlalchemy.exc import IntegrityError
 from src.database import create_postgres_session, create_postgres
 import json
@@ -11,6 +11,12 @@ with open('src/stations_data.json', 'r') as f:
 
 with open('src/region_data.json', 'r') as f:
     region_data = json.load(f)
+
+with open('src/weather_data.json', 'r') as f:
+    weather_data = json.load(f)
+
+with open('src/pattern_data.json', 'r') as f:
+    pattern_data = json.load(f)
 
 
 def create_stations(postgres_session, station_data = stations_data):
@@ -48,9 +54,13 @@ def create_region(postgres_session, region_data = region_data):
                 new_region = Regions(
                     id = region_id,
                     name = region_info['name'],
-                    latitude = region_info['latitude'],
-                    longitude = region_info['longitude'],
-                    region_code = region_info['region_code']
+                    region_code = region_info['region_code'],
+                    latitude_north = region_info['latitude_north'],
+                    latitude_south = region_info['latitude_south'],
+                    longitude_east = region_info['longitude_east'],
+                    longitude_west = region_info['longitude_west'],
+                    has_weather_data = region_info['has_weather_data'],
+                    has_pattern_station = region_info['has_pattern_station']
                 )
                 postgres_session.add(new_region)
                 postgres_session.commit()
@@ -59,9 +69,58 @@ def create_region(postgres_session, region_data = region_data):
                 print(f'Skipping region creation due to missing or invalid data: {e}')
             except IntegrityError:
                 postgres_session.rollback()
-                print(f"Failed to create station with ID '{region_id}. It may already exist.")
+                print(f"Failed to create region with ID '{region_id}. It may already exist.")
         else:
             print(f"Region with ID '{region_id} already exists. Skipping creation.")
+
+def create_weather_stations(postgres_session, weather_data = weather_data):
+    existing_weather_ids = {weather.id for weather in postgres_session.query(WeatherStations).all()}
+    for weather_info in weather_data:
+        weather_id = weather_info.get('id')
+        if weather_id not in existing_weather_ids:
+            try:
+                new_weather_station = WeatherStations(
+                    id = weather_id,
+                    name = weather_info['name'],
+                    latitude = weather_info['latitude'],
+                    longitude = weather_info['longitude'],
+                    region = weather_info['region']
+                )
+                postgres_session.add(new_weather_station)
+                postgres_session.commit()
+                print(f"Weather Station '{new_weather_station.name}' created successfully.")
+            except KeyError as e:
+                print(f'Skipping Weather Station creation due to missing or invalid data: {e}')
+            except IntegrityError:
+                postgres_session.rollback()
+                print(f"Failed to create Weather Station with ID '{weather_id}. It may already exist.")
+        else:
+            print(f"Weather Station with ID '{weather_id} already exists. Skipping creation.")
+
+def create_pattern_stations(postgres_session, pattern_data = pattern_data):
+    existing_pattern_ids = {pattern.id for pattern in postgres_session.query(PatternStations).all()}
+    for pattern_info in pattern_data:
+        pattern_id = pattern_info.get('id')
+        if pattern_id not in existing_pattern_ids:
+            try:
+                new_pattern_station = PatternStations(
+                    id = pattern_id,
+                    name = pattern_info['name'],
+                    bbox = pattern_info['bbox'],
+                    region = pattern_info['region']
+                )
+                postgres_session.add(new_pattern_station)
+                postgres_session.commit()
+                print(f"Pattern Station '{new_pattern_station.name}' created successfully.")
+            except KeyError as e:
+                print(f'Skipping Pattern Station creation due to missing or invalid data: {e}')
+            except IntegrityError:
+                postgres_session.rollback()
+                print(f"Failed to create Pattern Station with ID '{pattern_id}. It may already exist.")
+        else:
+            print(f"Pattern Station with ID '{pattern_id} already exists. Skipping creation.")
+
+
 
 def create_postgres_tables():
     try:
@@ -69,8 +128,10 @@ def create_postgres_tables():
         BasePostgres.metadata.create_all(postgres_engine)
         with create_postgres_session(postgres_engine) as session:
             create_region(postgres_session=session)
+            create_weather_stations(postgres_session=session)
+            create_pattern_stations(postgres_session=session)
             create_stations(postgres_session=session)
     except Exception as e:
-        logging.error(f'An error occurred: {e}')
+        logging.error(f'An error occurred in create_postgres_tables: {e}')
     finally:
         session.close()
