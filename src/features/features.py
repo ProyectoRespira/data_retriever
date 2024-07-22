9b6cbdb4967ef370912c6810dc230bd07eda08d4
vnbl <fernanda.carles@gmail.com>
2024-07-22 16:01:10 -0400

    run aqi calculation code

--- src/features/features.py
+++ src/features/features.py
@@ -1,4 +1,5 @@
 from src.features.utils import transform_raw_readings_to_station_readings, upsert_station_readings_into_db
+from src.features.aqi import compute_and_update_aqi_for_station_readings
 from src.database import create_postgres, create_postgres_session
 from src.querys import fetch_station_ids
 import logging
@@ -12,8 +13,20 @@ def transform_features():
         with create_postgres_session(postgres_engine) as session:
             stations = fetch_station_ids(session)
             for station_id in stations:
-                df = transform_raw_readings_to_station_readings(session, station_id)
-                upsert_station_readings_into_db(session, df)
+                logging.info(f'Starting basic transformations for station {station_id}')
+                
+                df, message = transform_raw_readings_to_station_readings(session, station_id)
+                if df is None:
+                    logging.warning(message)
+                    return True
+                else:
+                    logging.info(message)
+                
+                status = upsert_station_readings_into_db(session, df)
+                if status is False:
+                    logging.warning(f'Could not upsert station readings from station {station_id}')
+                # calculate and add AQI
+                compute_and_update_aqi_for_station_readings(session, station_id)
             return True
     
     except Exception as e:
