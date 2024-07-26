5bf4eaaede33b8feeefa6a10fff6e0992514a007
quanturtle <alvaromachucar@gmail.com>
2024-07-26 16:23:45 -0400

    added mage project

--- etl-process/etl_process/jobs.py
+++
@@ -1,4 +0,0 @@
-from dagster import define_asset_job, ScheduleDefinition
-from etl_process.assets import create_tables, select_data, transform_data, load_data
-
-run_pipeline_job = define_asset_job("run_pipeline", [create_tables, select_data, transform_data, load_data])
--- etl-process/etl_process/resources.py
+++
@@ -1,8 +0,0 @@
-from dagster import ConfigurableResource
-
-
-class SomeCredentials(ConfigurableResource):
-    user: str
-    password: str
-    host: str
-    
