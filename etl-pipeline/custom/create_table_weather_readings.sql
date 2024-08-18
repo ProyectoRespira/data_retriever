28fa5f05db955c619cb5a46773f0ee5ffe270a18
vnbl <fernanda.carles@gmail.com>
2024-08-18 01:25:42 -0400

    Replaced reserved keywords in table column definitions with unreserved alternatives

--- etl-pipeline/custom/create_table_weather_readings.sql
+++ etl-pipeline/custom/create_table_weather_readings.sql
@@ -1,11 +1,22 @@
-CREATE TABLE IF NOT EXISTS weather_readings (
+CREATE TABLE IF NOT EXISTS weather_readings_gold (
     id SERIAL PRIMARY KEY,
     weather_station INTEGER REFERENCES weather_stations(id),
-    date TIMESTAMP,
+    date_localtime TIMESTAMP,
     temperature FLOAT,
     humidity FLOAT,
     pressure FLOAT,
     wind_speed FLOAT,
     wind_dir_cos FLOAT,
     wind_dir_sin FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS weather_readings_silver (
+    id SERIAL PRIMARY KEY,
+    weather_station INTEGER REFERENCES weather_stations(id),
+    date_utc TIMESTAMP,
+    temperature FLOAT,
+    humidity FLOAT,
+    pressure FLOAT,
+    wind_speed FLOAT,
+    wind_dir FLOAT
 );
