adbf3a1b74b17346c7eb8956a9e16f4681c9a1bd
vnbl <fernanda.carles@gmail.com>
2024-08-19 21:03:01 -0400

    fixed airnow bug

--- etl-pipeline/data_loaders/extract_new_readings_from_airnow_silver.sql
+++ etl-pipeline/data_loaders/extract_new_readings_from_airnow_silver.sql
@@ -3,6 +3,9 @@ FROM airnow_readings_silver silver
 WHERE silver.date_utc > COALESCE((
     SELECT MAX(gold.date_localtime AT TIME ZONE 'America/Asuncion' AT TIME ZONE 'UTC')
     FROM station_readings_gold gold
-    JOIN stations s ON gold.station = s.id
-    WHERE s.is_pattern_station = TRUE
+    WHERE gold.station IN (
+        SELECT s.id
+        FROM stations s
+        WHERE s.is_pattern_station = TRUE
+    )
 ), '1970-01-01 00:00:00'::TIMESTAMP);
