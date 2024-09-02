0c44e7bdc566d895f4f2b9b988c3c937888da16d
vnbl <fernanda.carles@gmail.com>
2024-09-02 16:51:51 -0400

    improved readability - space between functions

--- etl-pipeline/transformers/transform_fiuna_silver_fill.py
+++ etl-pipeline/transformers/transform_fiuna_silver_fill.py
@@ -17,31 +17,29 @@ def add_date_column_as_index(df):
     df.set_index('date_localtime', inplace = True)
     return df.drop(columns = ['date', 'hour'], axis = 1)
 
+
 def resample_to_5min(df):
     df = df[~df.index.duplicated(keep='first')]
     df_resampled = df.resample('5min').asfreq()
     return df_resampled
 
+
 def fill_missing_values(df, columns):
     df_interpolated = df.copy()
     df_interpolated[columns] = df[columns].interpolate(method = 'linear')
     df_interpolated['station_id'] = df_interpolated['station_id'].bfill().ffill()
-
     return df_interpolated
+
 
 def process_data(df):
     df = add_date_column_as_index(df)
-    print(df.head())
     df = resample_to_5min(df)
-
     columns = ['pm1', 'pm2_5', 'pm10', 'temperature', 'humidity', 'pressure']
     df = fill_missing_values(df, columns)
     df['measurement_id'] = df['measurement_id'].fillna(0).astype(int)
     df['station_id'] = df['station_id'].astype(int)
     df.reset_index(inplace = True)
-    
     return df
-
 
 
 @transformer
