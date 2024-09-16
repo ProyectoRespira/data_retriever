c977c71826def690dec799c8ddba64d5da844f58
vnbl <fernanda.carles@gmail.com>
2024-09-16 12:33:54 -0400

    deleted unused files: used before to fetch meteostat and airnow readings by max date. now handled by id

--- etl-pipeline/custom/select_last_reading_from_weather_silver.sql
+++
@@ -1,8 +0,0 @@
--- Docs: https://docs.mage.ai/guides/sql-blocks
-SELECT silver.*
-FROM weather_readings_silver silver
-WHERE silver.date_utc = (
-    SELECT MAX(s.date_utc)
-    FROM weather_readings_silver s
-    WHERE s.weather_station = silver.weather_station
-);
