3959fb0b7dd300b774786b1d9a62ed461e5e0bb6
vnbl <fernanda.carles@gmail.com>
2024-09-20 01:50:29 -0400

    etl_region_readings pipeline: working

---
+++ etl-pipeline/data_loaders/extract_data_for_regional_stats.sql
@@ -0,0 +1,46 @@
+-- Docs: https://docs.mage.ai/guides/sql-blocks
+WITH unprocessed_readings AS (
+    -- Select new data that hasn't been processed yet and exclude pattern stations
+    SELECT
+        gold.id,
+        gold.date_utc,
+        gold.station,
+        gold.pm2_5,
+        gold.aqi_pm2_5,
+        gold.aqi_level,
+        s.region
+    FROM station_readings_gold gold
+    JOIN stations s ON gold.station = s.id
+    WHERE 
+        gold.processed_to_region = FALSE
+        AND s.region = 'GRAN_ASUNCION'
+        AND s.is_pattern_station = FALSE
+),
+existing_readings AS (
+    -- Select previously processed data for the same `date_utc` as the unprocessed data, excluding pattern stations
+    SELECT
+        gold.id,
+        gold.date_utc,
+        gold.station,
+        gold.pm2_5,
+        gold.aqi_pm2_5,
+        gold.aqi_level,
+        s.region
+    FROM station_readings_gold gold
+    JOIN stations s ON gold.station = s.id
+    WHERE 
+        gold.processed_to_region = TRUE
+        AND s.region = 'GRAN_ASUNCION'
+        AND s.is_pattern_station = FALSE
+        AND gold.date_utc IN (
+            SELECT DISTINCT unprocessed.date_utc
+            FROM unprocessed_readings unprocessed
+        )
+)
+-- Union the new unprocessed readings with the existing processed readings for recalculation
+SELECT *
+FROM unprocessed_readings
+UNION
+SELECT *
+FROM existing_readings
+ORDER BY date_utc ASC;
