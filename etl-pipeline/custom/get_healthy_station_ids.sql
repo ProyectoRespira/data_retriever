c7cf681c9080e977be93eb04f01ae0ebc897b05a
vnbl <fernanda.carles@gmail.com>
2024-11-27 17:07:25 -0300

    inference optimization: using for loop insted of dynamic child - now loading models only once

--- etl-pipeline/custom/get_healthy_station_ids.sql
+++ etl-pipeline/custom/get_healthy_station_ids.sql
@@ -1,15 +1,15 @@
 -- Docs: https://docs.mage.ai/guides/sql-blocks
+
 WITH execution_date AS (
     SELECT run_date
     FROM inference_runs
     ORDER BY id DESC
     LIMIT 1
 )
-
 SELECT
-    station_id as id
+    station_id AS id
 FROM health_checks
 WHERE 
-    run_date = (SELECT date_trunc('hour', run_date) - INTERVAL '1 HOUR' FROM execution_date)
+    run_date = (SELECT date_trunc('hour', run_date) FROM execution_date)
     AND is_on = TRUE
     AND station_id NOT IN (SELECT id FROM stations WHERE is_pattern_station = TRUE);
